9946415cc4026eca8dbdbd7cc5bb26ee9dab95ac
JULIUS AALTO <72438387+0xjulius@users.noreply.github.com>
2024-06-30 08:18:22 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Galleria from './components/Galleria.jsx';
+export default function App() {
+  return (
+  <>
+    <h1 className="text-center font-bold">Image Gallery</h1>
+    <h2 className="text-center"></h2>
+    <Galleria/>
+  </>
+  )
+}
