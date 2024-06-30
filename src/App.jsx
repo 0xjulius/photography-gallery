b1c6e4111b056b089907bc4968307b28af3e47a2
JULIUS AALTO <72438387+0xjulius@users.noreply.github.com>
2024-06-30 08:19:44 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Galleria from './components/Galleria.jsx';
 export default function App() {
   return (
   <>
-    <h1 className="text-center font-bold">Image Gallery</h1>
+    <h1 className="text-center font-bold">Image Gallery 1.0</h1>
     <h2 className="text-center"></h2>
     <Galleria/>
   </>
