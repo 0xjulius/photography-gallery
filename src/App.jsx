9aa026a6172bcaa1ad79252fde2b79d56acd424e
JULIUS AALTO <72438387+0xjulius@users.noreply.github.com>
2024-07-01 04:32:25 +0300

    update 2.3
    added favicon
    added h2 text header
    changed site title

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import Galleria from './components/Galleria.jsx';
 export default function App() {
   return (
   <>
-    <h1 className="text-center text-gradient text-7xl lg:text-8xl p-10 lg:p-12 animate__animated animate__fadeIn animate__delay-1s">visionbyjulius</h1>
-    <h2 className="text-center"></h2>
+    <h1 className="text-center font-semibold text-gradient text-7xl lg:text-8xl p-6 lg:p-12 animate__animated animate__fadeIn animate__delay-1s">visionbyjulius.</h1>
+    <h2 className="text-center text-xl text-white text-opacity-75 pb-10">Welcome to my photography gallery!</h2>
     <Galleria/>
   </>
   )
