91c6d9ad3f717a37ff7db94782edb03c27b64b47
JULIUS AALTO <72438387+0xjulius@users.noreply.github.com>
2024-07-01 04:38:36 +0300

    update 2.5
    changed padding on h1, h2

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import Galleria from "./components/Galleria.jsx";
 export default function App() {
   return (
     <>
-      <h1 className="text-center font-semibold text-gradient text-6xl lg:text-8xl p-6 lg:p-12 animate__animated animate__fadeIn animate__delay-1s">
+      <h1 className="text-center font-semibold text-gradient text-6xl lg:text-8xl p-6 lg:p-12 animate__animated animate__fadeIn animate__delay-1s pt-20">
         visionbyjulius.
       </h1>
-      <h2 className="text-center text-xl text-white text-opacity-75 pb-10">
+      <h2 className="text-center text-xl text-white text-opacity-75 pb-20">
         Welcome to my photography gallery!
       </h2>
       <Galleria />
