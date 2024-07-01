0ddb69a3bd4ec6a61464abc174ed0d790532dca5
JULIUS AALTO <72438387+0xjulius@users.noreply.github.com>
2024-07-01 04:36:17 +0300

    update 2.4
    changed h1 sizing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import Galleria from './components/Galleria.jsx';
+import Galleria from "./components/Galleria.jsx";
 export default function App() {
   return (
-  <>
-    <h1 className="text-center font-semibold text-gradient text-7xl lg:text-8xl p-6 lg:p-12 animate__animated animate__fadeIn animate__delay-1s">visionbyjulius.</h1>
-    <h2 className="text-center text-xl text-white text-opacity-75 pb-10">Welcome to my photography gallery!</h2>
-    <Galleria/>
-  </>
-  )
+    <>
+      <h1 className="text-center font-semibold text-gradient text-6xl lg:text-8xl p-6 lg:p-12 animate__animated animate__fadeIn animate__delay-1s">
+        visionbyjulius.
+      </h1>
+      <h2 className="text-center text-xl text-white text-opacity-75 pb-10">
+        Welcome to my photography gallery!
+      </h2>
+      <Galleria />
+    </>
+  );
 }
