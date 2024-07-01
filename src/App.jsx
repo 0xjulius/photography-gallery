fcf7c341df4fb48d39188b1aa49713252e7469b6
JULIUS AALTO <72438387+0xjulius@users.noreply.github.com>
2024-07-01 04:40:02 +0300

    update 2.6
    added animation + delay for h2

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
       <h1 className="text-center font-semibold text-gradient text-6xl lg:text-8xl p-6 lg:p-12 animate__animated animate__fadeIn animate__delay-1s pt-20">
         visionbyjulius.
       </h1>
-      <h2 className="text-center text-xl text-white text-opacity-75 pb-20">
+      <h2 className="text-center text-xl text-white text-opacity-75 pb-20 animate__animated animate__fadeIn animate__delay-2s">
         Welcome to my photography gallery!
       </h2>
       <Galleria />
